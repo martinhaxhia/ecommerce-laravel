2581e87008d147d634beb8276fc09ec6e7d3561e
Martin  Haxhia <martin.haxhia@atis.al>
2022-04-26 11:29:15 +0200

    new variable

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use App\Http\Requests\StoreProductRequest;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function productList()
+    {
+        $products = Product::all();
+        return view('products', compact('products'));
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $name = $request->name;
+        return view('test', [
+            'key' => $name
+        ]);
+    }
+protected $test;    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create(array $data)
+    {
+        return Product::create([
+            'productName' => $data['productName'],
+            'price' => $data['price'],
+            'description' => $data['description'],
+            'image' => $data['image'],
+          ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreProductRequest $request)
+    {
+        $this->validate(request(), [
+            'productName' => 'required',
+            'description' => 'required',
+            'price' => 'required|number'
+        ]);
+
+        $product = Product::create(request(['productName', 'description', 'price']));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/order.blade.php
@@ -0,0 +1,46 @@
+@extends('frontend')
+
+@section('content')
+        <main class="my-8">
+            <div class="container px-6 mx-auto">
+                <div class="flex justify-center my-6">
+                    <div class="flex flex-col w-full p-8 text-gray-800 bg-white shadow-lg pin-r pin-y md:w-4/5 lg:w-4/5">
+                      
+                          <div class="p-4 mb-3 bg-green-400 rounded">
+                              <p class="text-green-800">{{ $message }}</p>
+                          </div>
+                     
+                        <h3 class="text-3xl text-bold">Cart List</h3>
+                      <div class="flex-1">
+                        <table class="w-full text-sm lg:text-base" cellspacing="0">
+                         
+                          <thead>
+
+                            <tr class="h-12 uppercase">
+                              <th class="hidden md:table-cell"></th>
+                              <th class="text-left">Name</th>
+                              <th class="pl-5 text-left lg:text-right lg:pl-0">
+                                <span class="lg:hidden" title="Quantity">Qtd</span>
+                                <span class="hidden lg:inline">Quantity</span>
+                              </th>
+                              <th class="hidden text-right md:table-cell"> price</th>
+                              <th class="hidden text-right md:table-cell"> Remove </th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                          <tr class="h-12 uppercase">
+                              <th class="hidden md:table-cell"></th>
+                              <th class="text-left">Name</th>
+                              <th class="pl-5 text-left lg:text-right lg:pl-0">
+                                <span class="lg:hidden" title="Quantity">Qtd</span>
+                                <span class="hidden lg:inline">Quantity</span>
+                              </th>
+                              <th class="hidden text-right md:table-cell"> price</th>
+                              <th class="hidden text-right md:table-cell"> Remove </th>
+                            </tr>
+                          </tbody>
+                        </table>
+                        </div>
+                  </div>
+            </div>
+        </main>
