d086e458da3f5564c6ea9366eaaae35cbec19143
martinhaxhia <38227839+martinhaxhia@users.noreply.github.com>
2022-06-14 21:33:25 +0200

    create and edit

--- resources/views/admin/create.blade.php
+++ resources/views/admin/create.blade.php
@@ -5,44 +5,44 @@
         <div class="col-lg-12 margin-tb">
             <div class="pull-left">
                 <h3>Create Product</h3>
+                <div class="container">
+                    @if($errors->any())
+                        {!! implode('', $errors->all('<div>:message</div>')) !!}
+                    @endif
+                    <form action="{{route('products.store')}}" method="POST" enctype="multipart/form-data">
+                        @csrf
+                        <div class="mb-3 has-validation">
+                            <label for="name" class="form-label">Product name</label>
+                            <input name="name"  type="text" class="form-control" id="name" value="{{ old('name') }}" aria-describedby="name"/>
+                            @error('name')
+                            <div class="invalid">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <div class="mb-3">
+                            <label for="price" class="form-label">Product price</label>
+                            <input name="price"  type="number" class="form-control" id="price" value="{{ old('price') }}" aria-describedby="price"/>
+                            @error('price')
+                            <div class="invalid">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <div class="mb-3">
+                            <label for="description" class="form-label">Product description</label>
+                            <textarea name="description"  type="textarea" class="form-control" id="description" value="{{ old('description') }}" aria-describedby="description"></textarea>
+                            @error('description')
+                            <div class="invalid">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <div class="mb-3">
+                            <label for="image" class="form-label">Product image</label>
+                            <input name="image"  type="file" class="form-control" id="image" aria-describedby="image"/>
+                            @error('image')
+                            <div class="invalid">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="container">
-        @if($errors->any())
-            {!! implode('', $errors->all('<div>:message</div>')) !!}
-        @endif
-        <form action="{{route('products.store')}}" method="POST" enctype="multipart/form-data">
-            @csrf
-            <div class="mb-3 has-validation">
-                <label for="name" class="form-label">Product name</label>
-                <input name="name"  type="text" class="form-control" id="name" value="{{ old('name') }}" aria-describedby="name"/>
-                @error('name')
-                <div class="invalid">{{ $message }}</div>
-                @enderror
-            </div>
-            <div class="mb-3">
-                <label for="price" class="form-label">Product price</label>
-                <input name="price"  type="number" class="form-control" id="price" value="{{ old('price') }}" aria-describedby="price"/>
-                @error('price')
-                <div class="invalid">{{ $message }}</div>
-                @enderror
-            </div>
-            <div class="mb-3">
-                <label for="description" class="form-label">Product description</label>
-                <textarea name="description"  type="textarea" class="form-control" id="description" value="{{ old('description') }}" aria-describedby="description"></textarea>
-                @error('description')
-                <div class="invalid">{{ $message }}</div>
-                @enderror
-            </div>
-            <div class="mb-3">
-                <label for="image" class="form-label">Product image</label>
-                <input name="image"  type="file" class="form-control" id="image" aria-describedby="image"/>
-                @error('image')
-                <div class="invalid">{{ $message }}</div>
-                @enderror
-            </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
     </div>
 @endsection
--- resources/views/admin/products.blade.php
+++ resources/views/admin/products.blade.php
@@ -2,13 +2,13 @@
 
 @section('content')
     <main class="my-8">
+        @if(!$products->isEmpty())
         <div class="container px-6 mx-auto">
             <div class="w-full flex justify-center my-6">
                 <div class="flex flex-col w-full p-8 text-gray-800 bg-white shadow-lg pin-r pin-y md:w-4/5 lg:w-4/5">
                     <h3 class="text-3xl text-bold">Products </h3>
                     <div class="flex-1">
                         <table class="table" >
-                            @if(empty($item))
                                 <thead>
                                 <tr>
                                     <th>Image</th>
@@ -64,13 +64,14 @@
                                     </tr>
                                 @endforeach
                                 </tbody>
-                            @endif
                         </table>
                     </div>
                 </div>
                 <a class="btn btn-primary" href="{{ route('products.create') }}">Create New Product</a>
             </div>
         </div>
+        @endif
+    @if(!$trashed->isEmpty())
         <br>
         <div class="container px-6 mx-auto">
             <div class="w-full flex justify-center my-6">
@@ -117,5 +118,6 @@
                 <a class="btn btn-primary" href="{{ route('products.restoreAll') }}">Restore All Product</a>
             </div>
         </div>
+        @endif
     </main>
 @endsection
